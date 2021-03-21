2fefaa75b2277e6b2a52303b8b07b6fa26559376
Jonathan Wilson <jon.wils15@gmail.com>
2021-03-21 21:18:54 +0000

    set current date/time to all files in folder

--- dicom_dir.py
+++ dicom_dir.py
@@ -1,60 +1,52 @@
-import os.path
-from os.path import dirname, join
-from pprint import pprint
+import os
+from pathlib import Path
+from tempfile import TemporaryDirectory
+import warnings
 
-import pydicom
-from pydicom.data import get_testdata_files
-from pydicom.filereader import read_dicomdir
+from pydicom import dcmread
+from pydicom.data import get_testdata_file
+from pydicom.fileset import FileSet
+from pydicom.uid import generate_uid
 
-
-
-
-#pydicom.config.datetime_conversion= True
-
-# fetch the path to the test data
-#filepath=os.path.join(os.path.dirname(os.path.realpath(__file__)),"/raw_data", "/cube_dicom_files/")
-#filepath=os.path.abspath("C:\Users\Jonathan\Documents\Projects\Caristo_Test\DICOM_utility\raw_data\cube_dicom_files")
+import datetime
 
 
-filepath = "C:/Users/Jonathan/Documents/Projects/Caristo_Test/DICOM_utility/raw_data/cube_dicom_files/"
-#filepath = r"""C:\Users\Jonathan\Documents\Projects\Caristo_Test\DICOM_utility\raw_data\cube_dicom_files\ """
-filepath=filepath.replace("\\","/")
-print('Path to the DICOM directory: {}'.format(filepath))
-# load the data
-dicom_dir = read_dicomdir(filepath)
-base_dir = dirname(filepath)
 
 
+
+
+
+
+
+
+
+import os
 
 # Set current date/time
 dt = datetime.datetime.now()
 
 current_date= dt.strftime('%Y%m%d')
 current_time = dt.strftime('%H%M%S')
-
-
-# go through the patient record and print information
-for patient_record in dicom_dir.patient_records:
-    patient_record.ContentDate=current_date
-    patient_record.ContentTime=current_time
+print(current_date)
+print(current_time)
 
 
 
 
+folder_path = "C:/Users/Jonathan/Documents/Projects/Caristo_Test/DICOM_utility/raw_data/cube_dicom_files/"
+
+for file_name in os.listdir(folder_path):
+    if file_name.endswith(".dcm"): 
+        file_path=os.path.join(folder_path,file_name)
+        patient = dcmread(file_path)
+        print(patient.ContentDate)
+        print(patient.ContentTime)
+        patient.ContentDate=current_date
+        patient.ContentTime=current_time
+        #print(patient.ContentDate)
+        #print(patient.ContentTime)
+        
+        patient.save_as(file_path)
+        # print(os.path.join(directory, filename))
 
 
-
-# fetch the path to the test data
-path = r"""C:\Users\Jonathan\Documents\Projects\Caristo_Test\DICOM_utility\raw_data\cube_dicom_files"""
-ds = dcmread(path)
-root_dir = Path(ds.filename).resolve().parent
-print(f'Root directory: {root_dir}\n')
-
-
-
-
-# Iterate through the PATIENT records
-for patient in ds.patient_records:
-    patient.ContentDate=current_date
-    patient.ContentTime=current_time
-    
